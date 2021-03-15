5e75160e2b705b1e5f72f151ec817c46e9c27b12
ryanascherr <ryanascherr@gmail.com>
2021-03-15 08:24:20 -0400

    Created, styled, and completed functionality of saving throw section.

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var normal = document.querySelector("#normal");
 var advantage = document.querySelector("#advantage");
 var disadvantage = document.querySelector("#disadvantage");
 var skills = document.querySelector(".skills");
+var savingThrows = document.querySelector(".saving-throws");
 
 var proficiencyBonus = 5;
 var castingModifier = 5;
@@ -105,8 +106,40 @@ skills.addEventListener("click", function(event){
             document.getElementById("skill-roll-2").setAttribute("class", "plain");
         }
         document.getElementById("skill-roll-1").textContent = result1;
-        document.getElementById("skill-space").textContent = " | ";
+        document.getElementsByClassName("space")[0].textContent = " | ";
         document.getElementById("skill-roll-2").textContent = result2;
+    }
+})
+
+savingThrows.addEventListener("click", function(event){
+    var element = event.target;
+
+    if (element.matches(".saving-throw")) {
+        var modifier = element.getAttribute("data-modifier");
+        var modifierNumber = parseInt(modifier);
+        console.log(modifierNumber);
+        var roll1 = Math.floor(Math.random() * 20 + 1);
+        console.log(roll1);
+        var roll2 = Math.floor(Math.random() * 20 + 1);
+        console.log(roll2);
+        var result1 = roll1 + modifierNumber;
+        var result2 = roll2 + modifierNumber;
+        console.log(result1, result2);
+        if (roll1 === 1) {
+            document.getElementById("saving-throw-roll-1").setAttribute("class", "natural-1");
+        } else if (roll2 === 1) {
+            document.getElementById("saving-throw-roll-2").setAttribute("class", "natural-1");
+        } else if (roll1 === 20) {
+            document.getElementById("saving-throw-roll-1").setAttribute("class", "natural-20");
+        } else if (roll2 === 20) {
+            document.getElementById("saving-throw-roll-2").setAttribute("class", "natural-20");
+        } else {
+            document.getElementById("saving-throw-roll-1").setAttribute("class", "plain");
+            document.getElementById("saving-throw-roll-2").setAttribute("class", "plain");
+        }
+        document.getElementById("saving-throw-roll-1").textContent = result1;
+        document.getElementsByClassName("space")[1].textContent = " | ";
+        document.getElementById("saving-throw-roll-2").textContent = result2;
     }
 })
 
